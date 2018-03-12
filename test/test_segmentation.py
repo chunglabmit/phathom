aaf92a521056e120198a9ff59612fb3fae31019d
Lee Kamentsky <lee.kamentsky@gmail.com>
2018-03-12 15:35:46 -0400

    integrate tests into travis + use unittest

--- test/test_segmentation.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-class TestSegmentation(unittest.TestCase):
-    def test_00_00_import(self):
-        from phathom import segmentation
--- tests/tests.py
+++ tests/tests.py
@@ -1,16 +1,20 @@
 import phathom
 import numpy as np
+import unittest
+import os
+import tempfile
 
+class TestImageIO(unittest.TestCase):
+	def test_imread(self):
+		filename = os.path.join(os.path.split(__file__)[0], 'example.tif')
+		data = phathom.imageio.imread(filename)
+		self.assertEqual(data.shape, (64, 128, 128))
+		self.assertEqual(data.dtype, 'uint16')
 
-def test_imread():
-	filename = 'example.tif'
-	data = phathom.imageio.imread(filename)
-	assert data.shape == (64, 128, 128)
-	assert data.dtype == 'uint16'
-
-def test_imsave():
-	arr = np.random.random((32, 32, 32))
-	phathom.imageio.imsave('imsave_test.tif', arr)
-	tmp = phathom.imageio.imread('imsave_test.tif')
-	assert np.all(arr == tmp)
-	assert arr.dtype == tmp.dtype
+	def test_imsave(self):
+		arr = np.random.random((32, 32, 32))
+		filename = os.path.join(tempfile.gettempdir(), "imsave_test.tif")
+		phathom.imageio.imsave(filename, arr)
+		tmp = phathom.imageio.imread(filename)
+		assert np.all(arr == tmp)
+		assert arr.dtype == tmp.dtype
