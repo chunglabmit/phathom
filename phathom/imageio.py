ab83cc835d62992d06ab2cd5e383496c4b7a8fa0
Justin Swaney <jswaney@mit.edu>
2018-03-13 17:32:29 -0400

    Convert tabs to spaces for PEP8 compliance

--- phathom/imageio.py
+++ phathom/imageio.py
@@ -4,34 +4,39 @@ import zarr
 
 
 def imread(path):
-	"""
-	Loads TIF image into numpy array.
-	"""
-	return tifffile.imread(files=path)
+    """
+    Reads TIF file into a numpy array in memory.
+    :param path:
+    :return:
+    """
+    return tifffile.imread(files=path)
+
 
 def imsave(path, data):
-	"""
-	Saves numpy array as a TIF image.
-	"""
-	tifffile.imsave(file=path, data=data)
+    """
+    Saves numpy array as a TIF image.
+    """
+    tifffile.imsave(file=path, data=data)
 
-def tifs_to_zarr(tif_paths, zarr_path, chunks=True):
-	"""
-	Loads 2D TIF images into a (chunked) Zarr array one-by-one
-	"""
-	img = imread(tif_paths[0])
-	shape = (len(tif_paths), *img.shape)
-	dtype = img.dtype
-	z_arr = zarr.open(zarr_path, mode='w', shape=shape, chunks=chunks, dtype=dtype)
-	for z, tif_path in enumerate(tif_paths):
-		z_arr[z] = imread(tif_path)
-	return z_arr
+
+def tifs_to_zarr(tif_paths, zarr_path, chunks):
+    """
+    Loads 2D TIF images into a (chunked) Zarr array one-by-one
+    """
+    img = imread(tif_paths[0])
+    shape = (len(tif_paths), *img.shape)
+    dtype = img.dtype
+    z_arr = zarr.open(zarr_path, mode='w', shape=shape, chunks=chunks, dtype=dtype)
+    for z, tif_path in enumerate(tif_paths):
+        z_arr[z] = imread(tif_path)
+    return z_arr
+
 
 def main():
-	tif_paths, tif_filenames = utils.tifs_in_dir('data')
-	print(len(tif_paths))
-	tifs_to_zarr(tif_paths, 'data/data.zarr', (64, 128, 128))
+    tif_paths, tif_filenames = utils.tifs_in_dir('data')
+    print(len(tif_paths))
+    tifs_to_zarr(tif_paths, 'data/data.zarr', (64, 128, 128))
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- tests/tests.py
+++ tests/tests.py
@@ -4,17 +4,18 @@ import unittest
 import os
 import tempfile
 
-class TestImageIO(unittest.TestCase):
-	def test_imread(self):
-		filename = os.path.join(os.path.split(__file__)[0], 'example.tif')
-		data = phathom.imageio.imread(filename)
-		self.assertEqual(data.shape, (64, 128, 128))
-		self.assertEqual(data.dtype, 'uint16')
 
-	def test_imsave(self):
-		arr = np.random.random((32, 32, 32))
-		filename = os.path.join(tempfile.gettempdir(), "imsave_test.tif")
-		phathom.imageio.imsave(filename, arr)
-		tmp = phathom.imageio.imread(filename)
-		assert np.all(arr == tmp)
-		assert arr.dtype == tmp.dtype
+class TestImageIO(unittest.TestCase):
+    def test_imread(self):
+        filename = os.path.join(os.path.split(__file__)[0], 'example.tif')
+        data = phathom.imageio.imread(filename)
+        self.assertEqual(data.shape, (64, 128, 128))
+        self.assertEqual(data.dtype, 'uint16')
+
+    def test_imsave(self):
+        arr = np.random.random((32, 32, 32))
+        filename = os.path.join(tempfile.gettempdir(), "imsave_test.tif")
+        phathom.imageio.imsave(filename, arr)
+        tmp = phathom.imageio.imread(filename)
+        assert np.all(arr == tmp)
+        assert arr.dtype == tmp.dtype
