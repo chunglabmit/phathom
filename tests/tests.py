c9e174d7bc8e44e0709719b374d5ce22e2f21df1
Justin Swaney <jswaney@mit.edu>
2018-03-19 10:29:25 -0400

    Rename imageio to conversion in unit tests

--- tests/tests.py
+++ tests/tests.py
@@ -5,16 +5,16 @@ import os
 import tempfile
 
 
-class TestImageIO(unittest.TestCase):
+class TestConversion(unittest.TestCase):
     """
-    Unit tests for imageio module
+    Unit tests for conversion module
     """
     def test_imread(self):
         """
         Load example.tif using imread
         """
         filename = os.path.join(os.path.split(__file__)[0], 'example.tif')
-        data = phathom.imageio.imread(filename)
+        data = phathom.conversion.imread(filename)
         self.assertEqual(data.shape, (64, 128, 128), msg='loaded array has the wrong shape')
         self.assertEqual(data.dtype, 'uint16', msg='loaded array has the wrong data type')
 
@@ -24,8 +24,8 @@ class TestImageIO(unittest.TestCase):
         """
         arr = np.random.random((32, 32, 32))
         filename = os.path.join(tempfile.gettempdir(), "imsave_test.tif")
-        phathom.imageio.imsave(filename, arr)
-        tmp = phathom.imageio.imread(filename)
+        phathom.conversion.imsave(filename, arr)
+        tmp = phathom.conversion.imread(filename)
         self.assertTrue(np.all(arr == tmp), msg='saved and loaded array values are not equal')
         self.assertEqual(arr.dtype, tmp.dtype, msg='saved and loaded array do not have same data type')
 
